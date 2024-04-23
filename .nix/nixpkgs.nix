4d22fbf8867b37df0c2307142ba4087763bea7c9
Cyril Cohen <cohen@crans.org>
2024-04-23 10:44:59 +0200

    Pin a different version of nixpkgs

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+         url = https://github.com/sertel/nixpkgs/archive/1f2ff3c5fc4c4aade152ff69b909e46ff8a153a0.tar.gz;
+         sha256 = "047gh6lqa85982zq86l6cxlf7kgymx31i0wrzabnb2i04096cs6p";
+       }
